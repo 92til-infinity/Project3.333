7fa0d1b0bcb06c22c438a8d5763e0d2464950ee7
Scott Glover <glover.scott92@gmail.com>
2020-09-01 21:09:19 -0700

    .

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import App from './App';
+require('dotenv').config()
 
 ReactDOM.render(
   <React.StrictMode>
