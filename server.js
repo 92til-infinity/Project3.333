44a2baf259645c604ff624aafdcca24fd32b83aa
Scott Glover <glover.scott92@gmail.com>
2020-09-12 11:24:08 -0700

    testing, changed routes back to match ryans

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("*", (req, res) => {
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactBudgets",
+  process.env.MONGODB_URI || "mongodb://localhost/budgetss",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -41,6 +41,6 @@ mongoose.connection.on("connected", () => {
 });
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
