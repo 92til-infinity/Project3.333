d3457951ebce68d000fe8cc8f087853ab0498c94
Ryan Skog <ryan.skog@outlook.com>
2020-09-19 09:24:57 -0700

    Last minute fix to Profile page

--- client/src/components/Admin/homework.js
+++ client/src/components/Admin/homework.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 
 function Homework(props) {
   const [hwData, setHwData] = useState({
--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {
   MDBInput,
   MDBNavbar,
@@ -19,33 +19,24 @@ import {
   MDBRow,
   MDBCol,
   MDBBtn,
-} from 'mdbreact';
-import UserContext from '../../utils/UserContext';
-import './style.css';
+} from "mdbreact";
+import UserContext from "../../utils/UserContext";
+import "./style.css";
 
 const Profile = ({}) => {
   const { user, setUser } = React.useContext(UserContext);
   const [formData, setFormData] = useState({
-    firstname: '',
-    lastname: '',
-    email: '',
-    role: '',
-    facebook: '',
-    pinterest: '',
-    github: '',
-    twitter: '',
+    facebook: "",
+    pinterest: "",
+    github: "",
+    twitter: "",
   });
 
-  const {
-    firstname,
-    lastname,
-    email,
-    role,
-    facebook,
-    pinterest,
-    github,
-    twitter,
-  } = formData;
+  const { facebook, pinterest, github, twitter } = formData;
+
+  useEffect(() => {
+    setFormData(user.social);
+  }, []);
 
   const onChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -62,12 +53,13 @@ const Profile = ({}) => {
     try {
       const config = {
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       };
 
       const body = JSON.stringify(social);
       const res = await axios.put(`/api/users/${user._id}`, body, config);
+      setUser({ ...user, social });
       console.log(res.data);
     } catch (error) {
       console.error(error);
@@ -77,62 +69,62 @@ const Profile = ({}) => {
   return (
     <MDBContainer>
       <MDBRow>
-        <MDBCol md='12' className='py-5'>
+        <MDBCol md="12" className="py-5">
           <form onSubmit={(e) => onSubmit(e)}>
-            <h1 className='font-weight-bold mb-3 text-lg-left user'>
+            <h1 className="font-weight-bold mb-3 text-lg-left user">
               {user.firstname} {user.lastname}
             </h1>
-            <h2 className='font-weight-bold mb-3 text-lg-left'>Edit Profile</h2>
+            <h2 className="font-weight-bold mb-3 text-lg-left">Edit Profile</h2>
 
-            <div className='form-group social-input text-lg-left'>
-              <i className='fab fa-facebook fa-3x social' />
+            <div className="form-group social-input text-lg-left">
+              <i className="fab fa-facebook fa-3x social" />
               <MDBInput
-                className='social-text'
-                type='text'
+                className="social-text"
+                type="text"
                 placeholder={user.social.facebook}
-                name='facebook'
+                name="facebook"
                 value={facebook}
                 onChange={onChange}
               />
             </div>
 
-            <div className='form-group social-input text-lg-left'>
-              <i className='fab fa-pinterest fa-3x social' />
+            <div className="form-group social-input text-lg-left">
+              <i className="fab fa-pinterest fa-3x social" />
               <MDBInput
-                className='social-text'
-                type='text'
+                className="social-text"
+                type="text"
                 placeholder={user.social.pinterest}
-                name='pinterest'
+                name="pinterest"
                 value={pinterest}
                 onChange={onChange}
               />
             </div>
 
-            <div className='form-group social-input text-lg-left'>
-              <i className='fab fa-github fa-3x social' />
+            <div className="form-group social-input text-lg-left">
+              <i className="fab fa-github fa-3x social" />
               <MDBInput
-                className='social-text'
-                type='text'
+                className="social-text"
+                type="text"
                 placeholder={user.social.github}
-                name='github'
+                name="github"
                 value={github}
                 onChange={onChange}
               />
             </div>
 
-            <div className='form-group social-input text-lg-left'>
-              <i className='fab fa-twitter fa-3x social' />
+            <div className="form-group social-input text-lg-left">
+              <i className="fab fa-twitter fa-3x social" />
               <MDBInput
-                className='social-text'
-                type='text'
+                className="social-text"
+                type="text"
                 placeholder={user.social.twitter}
-                name='twitter'
+                name="twitter"
                 value={twitter}
                 onChange={onChange}
               />
             </div>
 
-            <MDBBtn type='submit' color='primary'>
+            <MDBBtn type="submit" color="primary">
               Update
             </MDBBtn>
           </form>
