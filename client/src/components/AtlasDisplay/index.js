8407ecc92d247b1f630558ecc457cef71fdea60b
avnoto <alexandravnoto@gmail.com>
2020-09-19 02:30:38 -0700

    budget styling

--- client/src/components/AtlasDisplay/index.js
+++ client/src/components/AtlasDisplay/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import Iframe from 'react-iframe';
+import './style.css';
 
 const AtlasDisplay = () => {
   return (
-    <div className='container' align='center' position='absolute'>
-      <h3 className='text-center'>Budget Display</h3>
+    <div className='container mt-0' align='center' position='absolute'>
+      {/* <h3 style={{ textAlign: 'match-parent' }}>Your Budget</h3> */}
       {/* code generated from MongoDBAtlas, inline styling prebuilt */}
       <Iframe
-        style={{
-          backgroundColor: '#21313C',
-          border: 'none',
-          borderRadius: '2px',
-          boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)',
-        }}
-        width='640px'
-        height='480px'
+        width='500px'
+        height='380px'
+        className='iframe'
         src='https://charts.mongodb.com/charts-project-0-ziumq/embed/charts?id=22bf0fe2-32cf-47db-937d-c4ac0540b06c&autoRefresh=45&theme=light'
       ></Iframe>
     </div>
   );
 };
+//
 
 export default AtlasDisplay;
