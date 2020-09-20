736434196a1c9e4cbf9368c7a2ae3905b7c3a02a
Scott Glover <glover.scott92@gmail.com>
2020-09-19 19:23:30 -0700

    budget delete button

--- client/src/components/ExpenseDisplay/index.js
+++ client/src/components/ExpenseDisplay/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { MDBTable, MDBTableBody, MDBTableHead } from 'mdbreact';
 import { BudgetConsumer } from '../../utils/BudgetContext';
 import API from '../../utils/API';
+
 class ExpenseDisplay extends Component {
+
   state = {
     expenses: [],
-  };
+    _id: 0
+  }
 
   componentDidMount() {
     API.getExpense().then((expenses) => {
@@ -13,62 +15,89 @@ class ExpenseDisplay extends Component {
       this.setState({ expenses: expenses.data });
     });
   }
+
+  deleteExpense = (id, dispatch) => {
+    API.deleteExpense(id)
+      .then((data) => {
+        console.log(data.data)
+        this.setState({
+          expenses: data.data,
+        });
+        dispatch({
+          type: 'expenses+',
+          expenses: this.state.expenses,
+        });
+
+      })
+      .catch(err => console.log(err));
+  };
+
+
   currentList = () => {
     const currentList =
       this.state.expenses.length > 0 ? (
         this.state.expenses.map((expense, index) => {
+          const { dispatch } = this.state;
           return (
             <tr key={index}>
               <td>{expense.expenseTitle}</td>
               <td>{expense.amount}</td>
               <td>{expense.category}</td>
+              <td>
+                <span className="delete-btn" role="button" id={expense._id} tabIndex="0" onClick={(e) => { this.deleteExpense(e.currentTarget.id) }}>
+                  ✗
+                </span>
+              </td>
             </tr>
           );
         })
       ) : (
-        <tr></tr>
-      );
+          <tr></tr>
+        );
     return currentList;
-  };
+  }
+
 
   render() {
     return (
-      <div>
-        <MDBTable>
-          <MDBTableHead color='primary-color' textWhite>
-            <tr style={{ backgroundColor: '#4285f4' }}>
-              <th>Title</th>
-              <th>Amount</th>
-              <th>Category</th>
+      <div className='card mt-5'>
+        <table className='table-bordered'>
+          <thead>
+            <tr>
+              <th>title</th>
+              <th>amount</th>
+              <th>category</th>
+              <th>remove</th>
             </tr>
-          </MDBTableHead>
+          </thead>
           <BudgetConsumer>
             {(value) => {
               // console.log(value.expenses);
               const expensesList =
                 value.expenses.length > 0 ? (
                   value.expenses.map((expense, index) => {
+                    const { dispatch } = this.state;
                     return (
                       <tr key={index}>
                         <td>{expense.expenseTitle}</td>
                         <td>{expense.amount}</td>
                         <td>{expense.category}</td>
+                        <td>
+                          <span className="delete-btn" role="button" id={expense._id} tabIndex="0" onClick={() => { this.deleteExpense(this.state._id) }}>
+                            ✗
+                        </span>
+                        </td>
                       </tr>
                     );
                   })
                 ) : (
-                  <tr></tr>
-                );
-              return (
-                <MDBTableBody>
-                  {this.currentList()}
-                  {expensesList}
-                </MDBTableBody>
-              );
+                    <tr></tr>
+                  );
+              return <tbody>{this.currentList()}{expensesList}</tbody>;
             }}
           </BudgetConsumer>
-          <MDBTableBody></MDBTableBody>
-        </MDBTable>
+          <tbody></tbody>
+        </table>
       </div>
     );
   }
